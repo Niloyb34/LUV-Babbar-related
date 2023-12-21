3bb26455d146d0e01d90a24fef95527ff233e4ca
Niloy Barua <98206229+Niloyb34@users.noreply.github.com>
2023-12-22 00:10:27 +0600

    Update GRAPH-Adjacency list using  graph class.cpp

--- GRAPH-Adjacency list using  graph class.cpp
+++ GRAPH-Adjacency list using  graph class.cpp
@@ -47,5 +47,20 @@ int main()
        }
        g.printGraph();
 }
-
+/*
+Enter the number of vertices : 5
+Enter the number of edges : 7
+0 1
+0 2
+1 2
+1 3
+2 3
+2 4
+3 4
+Adjacency list of vertex 0  is : 1 2
+Adjacency list of vertex 1  is : 0 2 3
+Adjacency list of vertex 2  is : 0 1 3 4
+Adjacency list of vertex 3  is : 1 2 4
+Adjacency list of vertex 4  is : 2 3
+*/
 
